caa4a43656345788dcc25d84baaf6b9e85bc16d4
codelikeleon <ls475@exeter.ac.uk>
2015-10-13 16:33:56 +0100

    Update Job.java

--- Job.java
+++ Job.java
@@ -9,7 +9,7 @@ public class Job implements Runnable {
 		System.out.println ("Job: " + jobNumber + " is being processed by thread : " + Thread.currentThread().getName());
 	try {
 		Thread.sleep((int) (1000));
-	} Catch (InterruptedException e) {}
+	} catch (InterruptedException e) {}
 		System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName());
 	}
 }
